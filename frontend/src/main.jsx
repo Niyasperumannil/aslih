5cd458590719190182621814c86dbb0a64d61560
Niyasperumannil <niyaskoottilakkadavu@gmail.com>
2025-08-01 14:16:19 +0530

    message

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,15 @@
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-// Fontsource for self-hosted Poppins font
-// import '@fontsource/poppins';         // Default weight 400
-// import '@fontsource/poppins/600.css';  // Optional: add weight 600
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // includes Popper
-
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <BrowserRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
- </BrowserRouter>
+  </BrowserRouter>
 );
